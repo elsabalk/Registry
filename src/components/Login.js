5aa97f58d66419c38be8bc8f85df6eb2559249a1
elsabalk <85199825+elsabalk@users.noreply.github.com>
2021-12-01 19:44:35 -0600

    Added Account creation page and refactored App.js to be account creation page first. Update login header to remove wedding date

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import Headers from "./LoginHeader.js";
 
-export default function App() {
+export default function Login() {
   const { register, handleSubmit } = useForm();
   const [result, setResult] = useState("");
   const onSubmit = (data) => setResult(JSON.stringify(data));
