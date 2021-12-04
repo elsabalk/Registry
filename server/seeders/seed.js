bd8f97e5643ea973c2cf292746e2b995c0005d83
Mohamed Ahmed <axmedmohamed7@gmail.com>
2021-12-04 12:34:43 -0600

    Revert "setup the gift/model gift seeds"

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,5 +1,1 @@
-const db = require('../config/connection');
-const { User,  Item} = require('../models');
-const userSeeds = require('./userSeeds.json');
-const items = require('./items.json');
-const userSeeds = require('./userSeeds.json');
+
