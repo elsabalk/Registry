e3b5175660de2d01a99650b00204b9ee72a80cf9
Mohamed Ahmed <axmedmohamed7@gmail.com>
2021-12-06 17:21:51 -0600

    added the model feilds

--- server/models/Item.js
+++ server/models/Item.js
@@ -1,25 +1,27 @@
-const { Schema, model } = require("mongoose");
+const { Schema } = require("mongoose");
 
+// This is a sub document schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const itemSchema = new Schema({
   title: {
     type: String,
-    required: "what is the title item!",
-    minlength: 1,
-    maxlength: 280,
-    trim: true,
-  },
-  img: {
-    type: String,
-  },
-  link: {
-    type: String,
     required: true,
   },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  category: {
+    type: String,
+  },
 });
-const Item = model("Thought", itemSchema);
 
-module.exports = Item;
-
+module.exports = itemSchema;
 /*
 title:DataTypes.STRING,
 img:DataTypes.STRING,
