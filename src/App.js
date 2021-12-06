068f712ba451804d9752efaceb3b0f77b231c55f
elsabalk <85199825+elsabalk@users.noreply.github.com>
2021-12-06 17:11:42 -0600

    Adding ProductList Page, adding product list css and updated index to start with product page( change back once back-end hooks up logic....)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import AccountCreate from './components/AccountCreate';
 import Login from './components/Login';
+import ProductList from './components/ProductList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-         <AccountCreate/>
+         <ProductList />
       </div>
     );
   }
