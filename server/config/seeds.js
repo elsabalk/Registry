f4fd65c2df961d6ab57c53e5ee73f60879d9dd6d
Mohamed Ahmed <axmedmohamed7@gmail.com>
2021-12-07 20:15:01 -0600

    unhandled promise

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,13 +1,1 @@
-const db = require('./connection')
-const {List, User} =require('../models');
-
-db.once('open', async ()=> {
-    await List.deleteMany();
-    const lists = await List.insertMany([
-        {
-            list: 'KitchenAid Stand Mixer',
-            list: 'HamiltonBeach Coffee Maker',
-            list: 'Engraved Marble Wine Chiller for Their Home Bar'
-        }
-    ])
-})
+const db = require('./connection');
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,5 @@
 const { User, List } = require('../models');
 const itemSchema = require('../models/Item');
 
-
-const resolvers = {
-}
-
 module.exports = resolvers;
   
